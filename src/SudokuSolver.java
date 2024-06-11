d1b27f556098ec4f7c3a6a458f5344e3a2d2017b
aakash123aman <aakashbhardwajg@gmail.com>
2024-06-11 07:37:31 -0700

    Add files via upload

---
+++ src/SudokuSolver.java
@@ -0,0 +1,93 @@
+public class SudokuSolver {
+
+    private static final int SIZE = 9;
+
+    public static void main(String[] args) {
+        int[][] sudokuGrid = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(sudokuGrid)) {
+            System.out.println("Sudoku solved successfully:");
+            printGrid(sudokuGrid);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    private static boolean solveSudoku(int[][] grid) {
+        int row = -1;
+        int col = -1;
+        boolean isEmpty = true;
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (grid[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    isEmpty = false;
+                    break;
+                }
+            }
+            if (!isEmpty) {
+                break;
+            }
+        }
+
+        if (isEmpty) {
+            return true;
+        }
+
+        for (int num = 1; num <= SIZE; num++) {
+            if (isValidMove(grid, row, col, num)) {
+                grid[row][col] = num;
+
+                if (solveSudoku(grid)) {
+                    return true;
+                } else {
+                    grid[row][col] = 0;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isValidMove(int[][] grid, int row, int col, int num) {
+        for (int x = 0; x < SIZE; x++) {
+            if (grid[row][x] == num || grid[x][col] == num) {
+                return false;
+            }
+        }
+
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[i + startRow][j + startCol] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static void printGrid(int[][] grid) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
